eec9a7e4e764bc66e69c981e86ced4aa38405461
EC2 Default User <ec2-user@ip-172-31-47-209.ap-northeast-1.compute.internal>
2022-08-29 09:48:00 +0000

    [start]4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,31 @@
 class BooksController < ApplicationController
   
-  def books
+  def new
+    @books = Book.new 
   end
+  
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+  
+  
+  def index
+    @books = Book.new 
+    @books = Book.all
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
